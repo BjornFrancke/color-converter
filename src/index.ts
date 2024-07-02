8d1f8c18df2a9d9a91c9a611e759be469214ced9
Bjorn Francke <bjorn@welcomebob.com>
2024-07-02 22:28:19 +0200

    Adds RGB to Hex color conversion

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 function hexValueToRGB(hex: string) {
     const hexValues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
     const RGB = []
+    if (hex.includes("#")) {
+        hex = hex.replace("#", "")
+    }
     let abbreviated = hex.length === 3;
 
     //if the hex is abbreviated split all characters instead of making pairs
@@ -22,4 +25,28 @@ function hexValueToRGB(hex: string) {
     return RGB
 }
 
-console.log(hexValueToRGB("FFF"))
+
+// (a*16) + (b * 1) = c
+
+function rgbToHexValue(red: number, green: number, blue: number) {
+    const hexValues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+
+    function colorToHex(color: number) {
+        for (let i = 0; i <= 15; i++) {
+            let value = i * 16
+            let remaining = color - value
+            if ((color - value) <= 16) {
+                //console.log(hexValues[i])
+                //console.log(hexValues[remaining])
+                return hexValues[i].toString() + hexValues[remaining].toString()
+            }
+
+        }
+        return "??"
+    }
+
+    return colorToHex(red) + colorToHex(green) + colorToHex(blue)
+
+}
+
+console.log(rgbToHexValue(255, 100, 255))
