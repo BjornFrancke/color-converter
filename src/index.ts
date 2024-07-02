9993bb972b8cf03ab3ee29b8f9c2a81339eabe15
Bjorn Francke <bjorn@welcomebob.com>
2024-07-02 21:50:56 +0200

    Adds support for abbreviated hex color codes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 function hexValueToRGB(hex: string) {
     const hexValues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
     const RGB = []
-    const hexRGB = hex.match(/.{1,2}/g) || []
-    //if (hexRGB.length === 2) {}
+    let abbreviated = hex.length === 3;
+
+    //if the hex is abbreviated split all characters instead of making pairs
+    const hexRGB = abbreviated ? hex.split("") : hex.match(/.{1,2}/g) || []
 
     for (let i = 0; i < 3; i++) {
         const stringTwo = hexRGB[i].split("")
@@ -10,10 +12,14 @@ function hexValueToRGB(hex: string) {
         for (let j = 0; j < stringTwo.length; j++) {
             colorCode.push(hexValues.indexOf(stringTwo[j]))
         }
-        colorCode[0] *= 16
-        RGB.push(colorCode[0] + colorCode[1])
+        if (abbreviated) {
+            RGB.push(colorCode[0] + (colorCode[0] * 16))
+        } else {
+            colorCode[0] *= 16
+            RGB.push(colorCode[0] + colorCode[1])
+        }
     }
-return RGB
+    return RGB
 }
 
-console.log(hexValueToRGB("FFFFFF"))
+console.log(hexValueToRGB("FFF"))
